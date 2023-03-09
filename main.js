408d705dfe5258a11b307271bfa6162f02bd1ae5
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2023-03-08 23:21:06 -0300

    refatoração

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-const subtrair = document.querySelector("#subtrair")
-const somar = document.querySelector("#somar")
-const braco = document.querySelector("#braco")
+const controle = document.querySelectorAll("[data-controle]")
 
-const controle = document.querySelectorAll(".controle-ajuste")
+controle.forEach ( (elemento) => {
+    elemento.addEventListener("click", (evento ) => {
+        manipulaDados(evento.target.dataset.controle , evento.target.parentNode)
 
-somar.addEventListener("click",() => {manipulaDados("somar")})
+    })
+} )
 
-subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
 
-function manipulaDados(operacao) {
-    if (operacao === "subtrair"){
-        braco.value = parseInt(braco.value) - 1
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador ]")
+
+    if (operacao === "-"){
+        peca.value = parseInt(peca.value) - 1
     } else{
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
 }
